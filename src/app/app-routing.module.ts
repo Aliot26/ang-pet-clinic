e53e23390d99072e4466cdbba8d6c7d8d9236b5e
Aliot26 <olgakochanowa@gmail.com>
2020-10-01 20:20:20 +0200

    added content to error page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {VetsComponent} from './vets/vets.component';
+import {ErrorComponent} from './error/error.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'vets', component: VetsComponent},
+  {path: '**', component: ErrorComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
