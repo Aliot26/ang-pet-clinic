31e937b7726ff2cccdfed2dd13d3ca5faa408671
Aliot26 <olgakochanowa@gmail.com>
2020-10-15 10:23:12 +0200

    fixed styles

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,10 +5,12 @@ import {ErrorComponent} from './error/error.component';
 import {WelcomeComponent} from './welcome/welcome.component';
 import {HttpClientModule} from '@angular/common/http';
 import {BrowserModule} from '@angular/platform-browser';
+import {LoginComponent} from './login/login.component';
 
 const routes: Routes = [
   {path: '', component: WelcomeComponent},
   {path: 'vets', component: VetsComponent},
+  {path: 'login', component: LoginComponent},
   {path: '**', component: ErrorComponent}
 ];
 
